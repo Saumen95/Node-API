e2cc57529c5938b40693d2227f0eb2e91b0c2c59
Saumen95 <saumenroy323@gmail.com>
2019-07-27 05:44:08 +0600

    Creating mySQL connection

--- App.js
+++ App.js
@@ -6,12 +6,41 @@ const app = express()
 const morgan = require('morgan')  //using morgan to know response time and request we made
 app.use(morgan('combined'))
 
+const mysql = require('mysql')
+
+
+// Creating MySQL route
+
+app.get("/user/: id", (req, res) => {
+    console.log("Fetching Database" + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'mysqlsampledatabase'
+    })
+
+    const customernumber = req.params.customerNumber
+    const queryString = "FROM customers SELECT customerNumber WHERE city = ?"
+    connection.query(queryString, [customerNumber], (err, rows, fields) => {
+
+        if (err) {
+            console.log("Failed to fetch" + err)
+            res.end()
+            return
+        }
+        console.log("Fetched data from SQL")
+        res.json(rows)
+    })
+})
+
+// Creating root
 app.get("/", (req, res) => {
     console.log('responding root rout')
     res.send("API testing")
 })
 
-// Creating routes
+// Creating User route
 
 app.get("/Books", (req, res) => {
     var bookdata = /home/Minix / REST - API / books.json
