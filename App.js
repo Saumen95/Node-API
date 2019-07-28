5c6e7381cbd8d58ae8f38ae9bc9ac1c3749e6bdb
Saumen95 <saumenroy323@gmail.com>
2019-07-28 10:12:29 +0600

    deployment with heroku in process

--- App.js
+++ App.js
@@ -2,11 +2,16 @@
 
 const express = require('express')
 const app = express()
+app.use(express.static('./public'))
 
 const morgan = require('morgan')  //using morgan to know response time and request we made
 app.use(morgan('combined'))
 
+
 const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended = false}))
 
 
 // Creating MySQL route
@@ -17,12 +22,12 @@ app.get("/user/: id", (req, res) => {
     const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        database: 'mysqlsampledatabase'
+        database: 'mysqlsampledatabase',
     })
 
     const customernumber = req.params.customerNumber
     const queryString = "FROM customers SELECT customerNumber WHERE city = ?"
-    connection.query(queryString, [customerNumber], (err, rows, fields) => {
+    connection.query(queryString, [customernumber], (err, rows, fields) => {
 
         if (err) {
             console.log("Failed to fetch" + err)
@@ -35,8 +40,9 @@ app.get("/user/: id", (req, res) => {
 })
 
 // Creating root
+
 app.get("/", (req, res) => {
-    console.log('responding root rout')
+    console.log('responding root route')
     res.send("API testing")
 })
 
@@ -44,8 +50,18 @@ app.get("/", (req, res) => {
 
 app.get("/Books", (req, res) => {
     var bookdata = /home/Minix / REST - API / books.json
-    res.json(bookdata)
+    req.json(bookdata)
     res.send("Books route created")
+})
+
+// Serving html form for searching books
+
+app.post('\my-handling-form-page', (req, res) => {
+    console.log('form is created')
+    console.log('User Name' + req.body.user_name)
+    console.log('user_email' + req.body.user_mail)
+    Console.log('book_name' + req.json.title + req.json.isbn)
+    res.end()
 })
 
 app.listen(3000, () => {                 // default port:3000 defined
